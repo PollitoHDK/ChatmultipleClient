a766ae14ab7dbedb8fcd7cf50e098066490cb6c8
Raul <raul.quigua2@gmail.com>
2024-10-05 13:07:51 -0500

    rebuild: javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.chatapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+
+    opens com.example.chatapp to javafx.fxml;
+    exports com.example.chatapp;
+}
